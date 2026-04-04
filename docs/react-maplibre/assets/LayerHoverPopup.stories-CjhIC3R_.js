115e68ef078633de60b9ba7bd677205fc5b3d502
dBitech <darcy@dbitech.ca>
2026-04-04 18:27:04 +0000

    Deploying to docs from  @ f0126950dae8dd78aad1ac257aa9839d58959373 🚀

---
+++ docs/react-maplibre/assets/LayerHoverPopup.stories-CjhIC3R_.js
@@ -0,0 +1,5 @@
+import{j as r}from"./index-BsvSi_AS.js";import{b9 as e}from"./style-BmzsxNry.js";import{m as t}from"./MapContextDecorator-Bdgy6_UF.js";import"./iframe-DEYO4RMf.js";import"./preload-helper-D9Z9MdNV.js";import"./index-COs8s5I2.js";const u={title:"Hooks/LayerHoverPopup",component:e,argTypes:{},decorators:t},p=()=>r.jsx(r.Fragment,{children:r.jsx(e,{getPopupContent:()=>"Popup content"})}),o=p.bind({});o.parameters={};o.args={};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => {
+  return <>
+            <LayerHoverPopup getPopupContent={() => 'Popup content'} />
+        </>;
+}`,...o.parameters?.docs?.source}}};const d=["ExampleConfig"];export{o as ExampleConfig,d as __namedExportsOrder,u as default};
